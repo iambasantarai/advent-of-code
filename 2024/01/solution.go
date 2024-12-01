eebceed57aa77fe37cf03e39fb688483725b6910
iambasantarai <iambasantarai@gmail.com>
2024-12-01 21:48:25 +0545

    Solve day 01, part 1

---
+++ 2024/01/solution.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sort"
+	"strconv"
+	"strings"
+
+	"aoc/utils"
+)
+
+func getTotalDistance(lines []string) int {
+	var lParts, rParts []int
+
+	for _, line := range lines {
+		parts := strings.Fields(line)
+
+		lNum, err := strconv.Atoi(parts[0])
+		if err != nil {
+			panic(err)
+		}
+
+		rNum, err := strconv.Atoi(parts[1])
+		if err != nil {
+			panic(err)
+		}
+
+		lParts = append(lParts, lNum)
+		rParts = append(rParts, rNum)
+	}
+
+	sort.Ints(lParts)
+	sort.Ints(rParts)
+
+	totalDistance := 0
+
+	for i := 0; i < len(lParts); i++ {
+		difference := lParts[i] - rParts[i]
+		if difference < 0 {
+			difference *= -1
+		}
+
+		totalDistance += difference
+	}
+
+	return totalDistance
+}
+
+func main() {
+	lines, err := utils.ReadFileLineByLine("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	totaldistance := getTotalDistance(lines)
+	fmt.Printf("[PART 1] total distance: %d\n", totaldistance)
+}
