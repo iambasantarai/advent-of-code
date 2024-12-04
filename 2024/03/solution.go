d0c6f1f0a05d270dfe3a772bcc218285982b5daa
iambasantarai <iambasantarai@gmail.com>
2024-12-04 19:55:22 +0545

    Solve day 03, part 2

--- 2024/03/solution.go
+++ 2024/03/solution.go
@@ -14,6 +14,11 @@ func getValidMulExpFromPattern(pattern string) [][]string {
 	return re.FindAllStringSubmatch(pattern, -1)
 }
 
+func getValidInstructedMulExpFromPattern(pattern string) [][]string {
+	re := regexp.MustCompile(`(?:mul\((\d+),(\d+)\)|do\(\)|don't\(\))`)
+	return re.FindAllStringSubmatch(pattern, -1)
+}
+
 func getMuls(matches [][]string) ([]int, error) {
 	var muls []int
 	for _, match := range matches {
@@ -59,6 +64,56 @@ func getTotalSumOfNormalMuls(lines []string) (int, error) {
 	return totalSum, nil
 }
 
+func getInstructedMuls(matches [][]string) ([]int, error) {
+	enabled := true
+	var muls []int
+
+	for _, match := range matches {
+		if match[0] == "do()" {
+			enabled = true
+		} else if match[0] == "don't()" {
+			enabled = false
+		} else {
+			if enabled {
+				num1Str, num2Str := match[1], match[2]
+
+				num1, err := strconv.Atoi(num1Str)
+				if err != nil {
+					return nil, err
+				}
+
+				num2, err := strconv.Atoi(num2Str)
+				if err != nil {
+					return nil, err
+				}
+
+				muls = append(muls, num1*num2)
+			}
+		}
+	}
+
+	return muls, nil
+}
+
+func getTotalSumOfInstructedMuls(lines []string) (int, error) {
+	totalSum := 0
+
+	for _, line := range lines {
+		matches := getValidInstructedMulExpFromPattern(line)
+
+		muls, err := getInstructedMuls(matches)
+		if err != nil {
+			return 0, err
+		}
+
+		for _, mul := range muls {
+			totalSum += mul
+		}
+
+	}
+	return totalSum, nil
+}
+
 func main() {
 	lines, err := utils.ReadFileLineByLine("./input.txt")
 	if err != nil {
@@ -70,4 +125,10 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("[PART 1] total sum: ", totalSum)
+
+	totalSumWithInstructions, err := getTotalSumOfInstructedMuls(lines)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("[PART 2] total sum: ", totalSumWithInstructions)
 }
