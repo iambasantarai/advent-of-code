1e797bdead2667fedb41e9b4f097b192ea1cdea3
iambasantarai <iambasantarai@gmail.com>
2024-12-05 22:10:44 +0545

    Solve day 05, part 2

--- 2024/05/solution.go
+++ 2024/05/solution.go
@@ -71,17 +71,82 @@ func isUpdateValid(update []int, rules [][]int) bool {
 	return true
 }
 
-func updatesFollowsRules(updates [][]int, rules [][]int) int {
+func topologicalSort(graph map[int][]int, inDegree map[int]int, nodes []int) []int {
+	var sorted []int
+	queue := []int{}
+
+	for _, node := range nodes {
+		if inDegree[node] == 0 {
+			queue = append(queue, node)
+		}
+	}
+
+	for len(queue) > 0 {
+		curr := queue[0]
+		queue = queue[1:]
+		sorted = append(sorted, curr)
+
+		for _, neighbor := range graph[curr] {
+			inDegree[neighbor]--
+			if inDegree[neighbor] == 0 {
+				queue = append(queue, neighbor)
+			}
+		}
+	}
+
+	return sorted
+}
+
+func orderCorrectly(update []int, rules [][]int) []int {
+	graph := make(map[int][]int)
+	inDegree := make(map[int]int)
+	pageSet := make(map[int]bool)
+
+	for _, page := range update {
+		pageSet[page] = true
+		inDegree[page] = 0
+	}
+
+	for _, rule := range rules {
+		x, y := rule[0], rule[1]
+		if pageSet[x] && pageSet[y] {
+			graph[x] = append(graph[x], y)
+			inDegree[y]++
+		}
+	}
+
+	sorted := topologicalSort(graph, inDegree, update)
+
+	seen := make(map[int]bool)
+	for _, page := range sorted {
+		seen[page] = true
+	}
+
+	for _, page := range update {
+		if !seen[page] {
+			sorted = append(sorted, page)
+		}
+	}
+
+	return sorted
+}
+
+func updatesFollowsRules(updates [][]int, rules [][]int) (int, int) {
 	sumOfMids := 0
+	sumOfOrderCorrectedMids := 0
 
 	for _, update := range updates {
 		if isUpdateValid(update, rules) {
 			midIndex := len(update) / 2
 			sumOfMids += update[midIndex]
+		} else {
+			orderCorrectedUpdate := orderCorrectly(update, rules)
+			midIndex := len(update) / 2
+			sumOfOrderCorrectedMids += orderCorrectedUpdate[midIndex]
 		}
 	}
 
-	return sumOfMids
+	return sumOfMids, sumOfOrderCorrectedMids
 }
 
 func main() {
@@ -95,7 +160,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	sumOfMids := updatesFollowsRules(updates, rules)
+	sumOfMids, sumOfOrderCorrectedMids := updatesFollowsRules(updates, rules)
 
 	fmt.Println("[PART1] sum of mids: ", sumOfMids)
+	fmt.Println("[PART2] sum of mids: ", sumOfOrderCorrectedMids)
 }
