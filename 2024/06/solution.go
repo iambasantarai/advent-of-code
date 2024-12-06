ad6286c756adae02e4334b423fbcee4a005204e9
iambasantarai <iambasantarai@gmail.com>
2024-12-06 23:18:55 +0545

    Solve day 06, part 2

--- 2024/06/solution.go
+++ 2024/06/solution.go
@@ -129,6 +129,52 @@ func patrol(labMap [][]string, gate Point) (int, map[Coordinate]int) {
 	}
 }
 
+func isLoop(input [][]string, gate Point) bool {
+	path := make(map[Point]struct{})
+	path2 := make(map[Coordinate]struct{})
+	current := gate
+	for {
+		if _, ok := path[current]; !ok {
+			path[current] = struct{}{}
+		} else {
+			return true
+		}
+
+		if _, ok := path2[Coordinate{current.x, current.y}]; !ok {
+			path2[Coordinate{current.x, current.y}] = struct{}{}
+		}
+
+		valid, newCurrent := findNextStep(input, current)
+		if !valid {
+			return false
+		}
+
+		current = newCurrent
+	}
+}
+
+func findNewObstacleCount(input [][]string, gate Point, path map[Coordinate]int) int {
+	count := 0
+	obstanceMap := make(map[Coordinate]struct{})
+	for step := range path {
+		if step.x == gate.x && step.y == gate.y {
+			continue
+		}
+		if input[step.y][step.x] == "." {
+
+			input[step.y][step.x] = "#"
+			if isLoop(input, gate) {
+				if _, ok := obstanceMap[step]; !ok {
+					count++
+					obstanceMap[step] = struct{}{}
+				}
+			}
+			input[step.y][step.x] = "."
+		}
+	}
+	return count
+}
+
 func main() {
 	lines, err := utils.ReadFileLineByLine("./input.txt")
 	if err != nil {
@@ -138,7 +184,9 @@ func main() {
 	labMap := build2DGrid(lines)
 	startingPoint := findStartingPoint(labMap)
 
-	visitedPositions, _ := patrol(labMap, startingPoint)
+	visitedPositions, path := patrol(labMap, startingPoint)
+	differentPosition := findNewObstacleCount(labMap, startingPoint, path)
 
 	fmt.Println("[PART 1] visited positions:", visitedPositions)
+	fmt.Println("[PART 2] different positions:", differentPosition)
 }
