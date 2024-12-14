0754b28d9a397e583c3aeac82e1f07aa053b59da
iambasantarai <iambasantarai@gmail.com>
2024-12-14 21:14:13 +0545

    Solve day 13, part 1

---
+++ 2024/13/solution.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"aoc/utils"
+)
+
+type Coordinates struct {
+	X, Y int
+}
+
+func main() {
+	lines, err := utils.ReadFileLineByLine("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	prizes, err := getPrizes(lines)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tokens := getTokenCount(prizes)
+	fmt.Println("[PART 1] tokens: ", tokens)
+}
+
+func fetchSliceOfIntsInString(s string) ([]int, error) {
+	var nums []int
+
+	re := regexp.MustCompile(`-?\d+`)
+	matches := re.FindAllString(s, -1)
+
+	for _, match := range matches {
+		num, err := strconv.Atoi(match)
+		if err != nil {
+			return nil, err
+		}
+		nums = append(nums, num)
+	}
+
+	return nums, nil
+}
+
+// Solve the system of equations using Cramer's Rule
+func solveLinearEquation(a, b, target Coordinates) (bool, Coordinates) {
+	det := a.X*b.Y - a.Y*b.X
+	if det == 0 {
+		return false, Coordinates{}
+	}
+
+	detX := target.X*b.Y - target.Y*b.X
+	detY := a.X*target.Y - a.Y*target.X
+
+	if detX%det != 0 || detY%det != 0 {
+		return false, Coordinates{}
+	}
+
+	x := detX / det
+	y := detY / det
+
+	return x >= 0 && y >= 0, Coordinates{x, y}
+}
+
+func getPrizes(input []string) ([][]Coordinates, error) {
+	var values []Coordinates
+	var prizes [][]Coordinates
+
+	for _, line := range input {
+		if len(line) == 0 {
+			continue
+		}
+		nums, err := fetchSliceOfIntsInString(line)
+		if err != nil {
+			return nil, err
+		}
+		values = append(values, Coordinates{nums[0], nums[1]})
+
+		if strings.Contains(line, "Prize") {
+			prizes = append(prizes, values)
+			values = []Coordinates{}
+		}
+	}
+
+	return prizes, nil
+}
+
+func getButtonPressesIfValid(a, b, final Coordinates, add int) (bool, Coordinates) {
+	final.X += add
+	final.Y += add
+
+	return solveLinearEquation(a, b, final)
+}
+
+func getTokenCount(input [][]Coordinates) int {
+	count := 0
+
+	for _, prize := range input {
+		isValid, tokens := getButtonPressesIfValid(prize[0], prize[1], prize[2], 0)
+		if isValid {
+			count += tokens.X*3 + tokens.Y
+		}
+
+	}
+
+	return count
+}
